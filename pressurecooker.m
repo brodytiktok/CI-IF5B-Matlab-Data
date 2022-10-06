d4d07c3079405a89d392e621daf2f14a1ed9dfd3
YandD9 <105333102+brodytiktok@users.noreply.github.com>
2022-10-06 14:53:53 +0700

    a

--- pressurecooker.m
+++ pressurecooker.m
@@ -4,10 +4,10 @@ clear all;
 a=newfis('pressurecooker')
 
 a=addvar(a, 'input', 'water' ,[0 2000]);
-a=addmf(a, 'input', 1, 'sedikit', 'trapmf',[0 0 200 1000]);
-%a=addmf(a, 'input', 1 , 'medium','trimf',[1000]);
-%a=addmf(a, 'input', 1 , 'cukup banyak','trimf',[1350]);
-%a=addmf(a, 'input', 1 , 'banyak','trimf',[1650]);
+a=addmf(a, 'input', 1, 'terlalu sedikit', 'trapmf',[0 0 200 400]);
+a=addmf(a, 'input', 1 , 'sedikit','trimf',[400 600 800]);
+a=addmf(a, 'input', 1 , 'medium','trimf',[800 1000 1200]);
+a=addmf(a, 'input', 1 , 'banyak','trimf',[1200 1400 1600]);
 a=addmf(a, 'input', 1 , 'terlalu banyak','trapmf',[800 1800 2000 2000]);
 
 figure;plotmf(a,'input', 1);
